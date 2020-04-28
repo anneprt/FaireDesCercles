a6e4d1f768fdc13b3ee0ec32092147970edcf1cc
Anne <patesdegeek@gmail.com>
2020-04-28 22:19:24 +0200

    exemple  heritage

---
+++ src/fr/abp/cours/FormerDesCercles.java
@@ -0,0 +1,23 @@
+package fr.abp.cours;
+
+public class FormerDesCercles {
+    public static void main(String[] args) {
+        Cercle A = new Cercle(5, 5);
+        A.afficher();
+        double p = A.perimetre();
+        System.out.println("Votre cercle a pour perimetre : " + p);
+        A.déplacer(5, 10);
+        System.out.println("Apres déplacement : ");
+        A.afficher();
+        A.agrandir(10);
+        System.out.println("Apres agrandissement : ");
+        A.afficher();
+        Forme F = new Forme(10, 10);
+        F.échangerAvec(A);
+        A.afficher();
+        F.afficher();
+        A.échangerAvec(F);
+        A.afficher();
+        F.afficher();
+    }
+}
